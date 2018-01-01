682fb5dd5d430b3c6e5c0ad69838bf6e5e1a9060
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-01 22:50:18 +1100

    Added ClassA

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -6,7 +6,12 @@ namespace Interface
     {
         static void Main(string[] args)
         {
-            ;
+            ClassA a = new ClassA();
+            a.MyNumber = 5;
+            a.InterfaceMethod();
+            Console.Read();
         }
     }
+
+
 }
